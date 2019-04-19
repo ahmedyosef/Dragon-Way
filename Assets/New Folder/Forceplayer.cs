0d26143f99b269229e620ebc1994320c5e055f01
ahmedyosef <33419399+ahmedyosef@users.noreply.github.com>
2019-04-19 05:46:51 +0200

    firist comit 19/4/2019

---
+++ Assets/New Folder/Forceplayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forceplayer : MonoBehaviour {
+	public Rigidbody rb;
+	 public float speed;
+    public float tilt;
+	// Use this for initialization
+	void Start () {
+		rb=gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				 
+	}
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        Vector3 movement = new Vector3 (moveHorizontal*speed, 0.0f, speed).normalized;
+		movement = transform.TransformDirection(movement);
+        rb.velocity = movement * speed;
+        rb.rotation = Quaternion.Euler ( moveVertical * tilt, 0.0f, moveHorizontal * -tilt);
+    }
+
+}
+// if (Input.GetButton("Fire1"))
+// 		 {
+// 			 rb.velocity = transform.up * 10;
+
+// 		 }
